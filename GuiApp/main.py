bcd40b37e4eef46327769ac51e8468bafccdb26c
Douglas Halse <4headmaster@gmail.com>
2024-11-30 21:43:55 +0100

    Enable kivy inspector for layout debugging

--- GuiApp/main.py
+++ GuiApp/main.py
@@ -1,34 +1,39 @@
-import sys, os, threading
-
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.modules import inspector
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.lang import Builder
-from kivy.config import Config
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '480')
 
 from widgets.snackAttackTrack import SnackAttackTrackWidget
-
 from database import createPatreonTable, closeDatabase
+
+# Size of Raspberry pi touchscreen
+Window.size = (800, 480)
+
 
 class ImageButton(ButtonBehavior, Image):
     pass
 
+
 class BoxLayoutButton(ButtonBehavior, BoxLayout):
     pass
+
 
 class snackAttackTrackApp(App):
     def build(self):
         self.title = 'Snack Attack Track'
         Builder.load_file("kv/main.kv")
         createPatreonTable()
-        return SnackAttackTrackWidget()
+        snackAttackTrackWidget = SnackAttackTrackWidget()
+        inspector.create_inspector(Window, snackAttackTrackWidget)
+        return snackAttackTrackWidget
 
     def on_stop(self):
         closeDatabase()
         return super().on_stop()
+
 
 if __name__ == '__main__':
     snackAttackTrackApp().run()
