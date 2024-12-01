b6de82e6c4e58c3885192cca0e1ccc9244f617ed
Douglas Halse <4headmaster@gmail.com>
2024-12-01 12:06:54 +0100

    Add User main page

--- GuiApp/database.py
+++ GuiApp/database.py
@@ -1,6 +1,14 @@
 import sqlite3
 
 
+class UserData:
+    def __init__(self, patronId, firstName, lastName, employeeID):
+        self.patronId = patronId
+        self.firstName = firstName
+        self.lastName = lastName
+        self.employeeID = employeeID
+
+
 def createAllTables():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
@@ -51,11 +59,32 @@ def addPatron(first_name, last_name, employee_id):
     conn.commit()
 
 
-def getAllPatrons():
+def getAllPatrons() -> list[UserData]:
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Patrons")
-    return cursor.fetchall()
+    sqlResult = cursor.fetchall()
+    userDataList = []
+    for userEntry in sqlResult:
+        patronId = userEntry[0]
+        firstName = userEntry[1]
+        lastName = userEntry[2]
+        employeeID = userEntry[3]
+        userData = UserData(patronId, firstName, lastName, employeeID)
+        userDataList.append(userData)
+    return userDataList
+
+
+def getPatronData(patronID: int) -> UserData:
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM Patrons WHERE PatronID = {patronID}")
+    sqlResult = cursor.fetchone()
+    patronId = sqlResult[0]
+    firstName = sqlResult[1]
+    lastName = sqlResult[2]
+    employeeID = sqlResult[3]
+    return UserData(patronId, firstName, lastName, employeeID)
 
 
 def closeDatabase():
---
+++ GuiApp/widgets/mainUserScreen.py
@@ -0,0 +1,69 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from database import getPatronData, UserData
+
+
+class BoxLayoutButton(ButtonBehavior, BoxLayout):
+    pass
+
+
+class MainUserScreenHeader(GridLayout):
+    def __init__(self, mainUserScreen, userData: UserData, **kwargs):
+        super().__init__(**kwargs)
+        self.mainUserScreen = mainUserScreen
+        self.ids["welcomeTextLabel"].text = f"Welcome {userData.firstName}!"
+
+    def onLogoutButtonPressed(self, *largs):
+        self.mainUserScreen.logout()
+
+    def OnWrenchPressed(self, *largs):
+        self.mainUserScreen.GoToSettingsScreen()
+
+
+class MainUserScreenBody(GridLayout):
+    def __init__(self, mainUserScreen, **kwargs):
+        super().__init__(**kwargs)
+        self.mainUserScreen = mainUserScreen
+
+    def onBuyButtonPressed(self, *largs):
+        self.mainUserScreen.GoToBuyScreen()
+
+    def onTopUpButtonPressed(self, *largs):
+        self.mainUserScreen.GoToTopUpScreen()
+
+    def onHistoryButtonPressed(self, *largs):
+        self.mainUserScreen.GoToHistoryScreen()
+
+
+class MainUserScreenWidget(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def setUserId(self, userId: UserData):
+        userData = getPatronData(userId)
+        header = MainUserScreenHeader(self, userData)
+        self.ids["mainUserScreenGridLayout"].add_widget(header)
+        body = MainUserScreenBody(self)
+        self.ids["mainUserScreenGridLayout"].add_widget(body)
+
+    def GoToBuyScreen(self):
+        print("Going to Buy screen")
+        # Use screen manager to go to buy screen
+
+    def GoToTopUpScreen(self):
+        print("Going to Top up screen")
+        # Use screen manager to go to buy screen
+
+    def GoToHistoryScreen(self):
+        print("Going to History screen")
+        # Use screen manager to go to buy screen
+
+    def GoToSettingsScreen(self):
+        print("Going to Settings screen")
+        # Use screen manager to go to settings screen
+
+    def logout(self):
+        self.ids["mainUserScreenGridLayout"].clear_widgets()
+        self.manager.current = "splashScreen"
