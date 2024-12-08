f3d40c3e0690c8de2c4d2a4f0d5db884165846cf
DianaPankoS <141326179+DianaPankoS@users.noreply.github.com>
2024-12-08 19:06:21 +0100

    Add background animation

--- GuiApp/widgets/splashScreen.py
+++ GuiApp/widgets/splashScreen.py
@@ -1,15 +1,9 @@
 from kivy.uix.screenmanager import Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.behaviors import ButtonBehavior
-
-
-class BoxLayoutButton(ButtonBehavior, BoxLayout):
-    pass
 
 
 class SplashScreenWidget(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def onPress(self):
+    def onPressed(self):
         self.manager.transitionToScreen("loginScreen")
