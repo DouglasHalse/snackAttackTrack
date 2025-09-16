43555c7bdaa3db6771e6012798596aa8457615c5
Douglas Halse <4headmaster@gmail.com>
2025-09-16 20:03:26 +0200

    feat(app): ✨ Make MainUserScreen more static

--- GuiApp/widgets/customScreenManager.py
+++ GuiApp/widgets/customScreenManager.py
@@ -1,13 +1,21 @@
 from database import SnackData, UserData, getPatronData
 from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.screenmanager import ScreenManager, SlideTransition
+from kivy.uix.screenmanager import (
+    Screen,
+    ScreenManager,
+    ScreenManagerException,
+    SlideTransition,
+)
 from RFIDReader import RFIDReader
+from widgets.GridLayoutScreen import GridLayoutScreen
 from widgets.settingsManager import SettingsManager
 
 # pylint: disable=too-many-instance-attributes
 
 
 class CustomScreenManager(ScreenManager):
+    logged_in_user = ObjectProperty(None, allownone=True)
+
     def __init__(self, settingsManager: SettingsManager):
         super().__init__()
         self._currentPatron: UserData = None
@@ -20,9 +28,11 @@ class CustomScreenManager(ScreenManager):
 
     def login(self, patronId):
         self._currentPatron = getPatronData(patronID=patronId)
+        self.logged_in_user = self._currentPatron
 
     def logout(self):
         self._currentPatron = None
+        self.logged_in_user = None
 
     def setPatronToEdit(self, patronToEdit: UserData):
         self._patronToEdit = patronToEdit
@@ -51,3 +61,47 @@ class CustomScreenManager(ScreenManager):
     def transitionToScreen(self, screenName, transitionDirection: str = "left"):
         self.transition = SlideTransition(direction=transitionDirection)
         self.current = screenName
+
+    def add_widget(self, widget, *args, **kwargs):
+        """
+        .. versionchanged:: 2.1.0
+            Renamed argument `screen` to `widget`.
+        """
+        if not isinstance(widget, (GridLayoutScreen, Screen)):
+            raise ScreenManagerException("ScreenManager accepts only Screen widget.")
+        if widget.manager:
+            if widget.manager is self:
+                raise ScreenManagerException(
+                    "Screen already managed by this ScreenManager (are you "
+                    "calling `switch_to` when you should be setting "
+                    "`current`?)"
+                )
+            raise ScreenManagerException(
+                "Screen already managed by another ScreenManager."
+            )
+        widget.manager = self
+        widget.bind(name=self._screen_name_changed)
+        self.screens.append(widget)
+        if self.current is None:
+            self.current = widget.name
+
+    def remove_widget(self, widget, *args, **kwargs):
+        if not isinstance(widget, (GridLayoutScreen, Screen)):
+            raise ScreenManagerException(
+                "ScreenManager uses remove_widget only for removing Screens."
+            )
+
+        if widget not in self.screens:
+            return
+
+        if self.current_screen == widget:
+            other = next(self)
+            if widget.name == other:
+                self.current = None
+                widget.parent.real_remove_widget(widget)
+            else:
+                self.current = other
+
+        widget.manager = None
+        widget.unbind(name=self._screen_name_changed)
+        self.screens.remove(widget)
