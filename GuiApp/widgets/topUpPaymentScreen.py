ef640ce200fe4bbdf1f56e830283ac9fedbc1885
Douglas Halse <4headmaster@gmail.com>
2025-09-19 13:35:09 +0200

    feat(app): ✨ Make TopUpPaymentScreen more static

--- GuiApp/widgets/topUpPaymentScreen.py
+++ GuiApp/widgets/topUpPaymentScreen.py
@@ -1,86 +1,66 @@
 from datetime import datetime
 
-from database import addCredits, addTopUpTransaction
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.image import Image
+from database import UserData, addCredits, addTopUpTransaction
 from qrcode import make as makeQRCode
-from widgets.customScreenManager import CustomScreenManager
-from widgets.headerBodyLayout import HeaderBodyScreen
+from widgets.GridLayoutScreen import GridLayoutScreen
 from widgets.popups.creditsAnimationPopup import CreditsAnimationPopup
 from widgets.settingsManager import SettingName
 
 
-class TopUpPaymentScreenContent(GridLayout):
-    def __init__(
-        self,
-        screenManager: CustomScreenManager,
-        amountToBePayed: float,
-        **kwargs,
-    ):
+class TopUpPaymentScreen(GridLayoutScreen):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.screenManager = screenManager
-        self.amountToBePayed = amountToBePayed
-        self.userData = self.screenManager.getCurrentPatron()
-        swishNumber = self.screenManager.settingsManager.get_setting_value(
+        self.userData: UserData = None
+        self.amount_to_be_payed: float = None
+        self.ids.header.bind(on_back_button_pressed=self.on_back)
+
+    def on_back(self, _):
+        self.manager.transitionToScreen(
+            "topUpAmountScreen", transitionDirection="right"
+        )
+
+    def setAmountToBePayed(self, amount: float):
+        self.amount_to_be_payed = amount
+
+    def on_pre_enter(self, *args):
+        self.userData = self.manager.getCurrentPatron()
+        swishNumber = self.manager.settingsManager.get_setting_value(
             settingName=SettingName.PAYMENT_SWISH_NUMBER
         )
         self.createQrCode(
-            amount=self.amountToBePayed,
+            amount=self.amount_to_be_payed,
             name=self.userData.firstName,
             swishNumber=swishNumber,
         )
-        image = Image(source="qr.png")
-        self.ids["qrCodeLayout"].add_widget(image)
-        image.reload()
+        self.ids["qrCodeImage"].source = "qr.png"
+        self.ids["qrCodeImage"].reload()
+        return super().on_pre_enter(*args)
 
     def onConfirm(self, *largs):
         addTopUpTransaction(
             patronID=self.userData.patronId,
             amountBeforeTransaction=self.userData.totalCredits,
-            amountAfterTransaction=self.userData.totalCredits + self.amountToBePayed,
+            amountAfterTransaction=self.userData.totalCredits + self.amount_to_be_payed,
             transactionDate=datetime.now(),
         )
-        addCredits(self.userData.patronId, self.amountToBePayed)
+        addCredits(self.userData.patronId, self.amount_to_be_payed)
 
         # Update current patron with new data
-        self.screenManager.refreshCurrentPatron()
+        self.manager.refreshCurrentPatron()
 
         CreditsAnimationPopup(
             title="Thank you for your top-up!",
             creditsBefore=self.userData.totalCredits,
-            creditsAfter=self.userData.totalCredits + self.amountToBePayed,
+            creditsAfter=self.userData.totalCredits + self.amount_to_be_payed,
         ).open()
 
-        self.screenManager.transitionToScreen(
-            "mainUserPage", transitionDirection="right"
-        )
+        self.manager.transitionToScreen("mainUserPage", transitionDirection="right")
 
     def onCancel(self, *largs):
-        self.screenManager.transitionToScreen(
-            "mainUserPage", transitionDirection="right"
-        )
+        self.manager.transitionToScreen("mainUserPage", transitionDirection="right")
 
     def createQrCode(self, amount: float, name: str, swishNumber: str):
         qrCode = makeQRCode(
             f"https://app.swish.nu/1/p/sw/?sw={swishNumber}&amt={amount:.2f}&cur=SEK&msg=Snack%20Attack%20Top-up%20for%20{name}&src=qr"
         )
         qrCode.save("qr.png")
-
-
-class TopUpPaymentScreen(HeaderBodyScreen):
-    def __init__(self, **kwargs):
-        super().__init__(previousScreen="topUpAmountScreen", **kwargs)
-        self.headerSuffix = "Top up payment screen"
-        self.amountToBePayed = 0.0
-
-    def setAmountToBePayed(self, amount: float):
-        self.amountToBePayed = amount
-
-    def on_pre_enter(self, *args):
-        super().on_pre_enter(*args)
-        self.body.add_widget(
-            TopUpPaymentScreenContent(
-                screenManager=self.manager,
-                amountToBePayed=self.amountToBePayed,
-            )
-        )
