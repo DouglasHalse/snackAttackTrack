26d4cc619bc7aaa25d75682fcb45eec396ec3777
Douglas Halse <4headmaster@gmail.com>
2025-11-14 09:27:27 +0100

    style(test): :art: Remove unused classes in pytest

--- GuiApp/tests/test_login.py
+++ GuiApp/tests/test_login.py
@@ -2,59 +2,54 @@ import asyncio
 
 import pytest
 
-# Fixtures are counted as redefine-outer-name
-# pylint: disable=redefined-outer-name
 
-# Disable too-many-public-methods for test class
-# pylint: disable=too-many-public-methods
+@pytest.mark.asyncio
+async def test_login_by_selecting_user(app_with_users):
+
+    assert app_with_users.screenManager.current == "splashScreen"
+    app_with_users.screenManager.current_screen.onPressed()
+    assert app_with_users.screenManager.current == "loginScreen"
+
+    usersOnScreen = app_with_users.screenManager.current_screen.ids[
+        "LoginScreenUserGridLayout"
+    ].children
+    assert len(usersOnScreen) > 0
+
+    for userButton in usersOnScreen:
+        if userButton.ids.userNameLabel.text == "User2FirstName":
+            userButton.ids.clickableLayout.dispatch("on_press")
+            break
+
+    assert app_with_users.screenManager.current == "mainUserPage"
+    assert (
+        "User2FirstName"
+        in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
+    )
 
 
-class TestLogin:
-    @pytest.mark.asyncio
-    async def test_login_by_selecting_user(self, app_with_users):
+@pytest.mark.asyncio
+async def test_login_from_splash_screen_with_card(app_with_users):
+    assert app_with_users.screenManager.current == "splashScreen"
 
-        assert app_with_users.screenManager.current == "splashScreen"
-        app_with_users.screenManager.current_screen.onPressed()
-        assert app_with_users.screenManager.current == "loginScreen"
+    app_with_users.screenManager.RFIDReader.triggerFakeRead(card_id=555555555)
 
-        usersOnScreen = app_with_users.screenManager.current_screen.ids[
-            "LoginScreenUserGridLayout"
-        ].children
-        assert len(usersOnScreen) > 0
+    assert app_with_users.screenManager.current == "mainUserPage"
+    assert (
+        "User3FirstName"
+        in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
+    )
 
-        for userButton in usersOnScreen:
-            if userButton.ids.userNameLabel.text == "User2FirstName":
-                userButton.ids.clickableLayout.dispatch("on_press")
-                break
 
-        assert app_with_users.screenManager.current == "mainUserPage"
-        assert (
-            "User2FirstName"
-            in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
-        )
+@pytest.mark.asyncio
+async def test_login_from_login_screen_with_card(app_with_users):
+    assert app_with_users.screenManager.current == "splashScreen"
+    app_with_users.screenManager.current_screen.onPressed()
 
-    @pytest.mark.asyncio
-    async def test_login_from_splash_screen_with_card(self, app_with_users):
-        assert app_with_users.screenManager.current == "splashScreen"
+    await asyncio.sleep(0.5)
+    app_with_users.screenManager.RFIDReader.triggerFakeRead(card_id=123456789)
 
-        app_with_users.screenManager.RFIDReader.triggerFakeRead(card_id=555555555)
-
-        assert app_with_users.screenManager.current == "mainUserPage"
-        assert (
-            "User3FirstName"
-            in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
-        )
-
-    @pytest.mark.asyncio
-    async def test_login_from_login_screen_with_card(self, app_with_users):
-        assert app_with_users.screenManager.current == "splashScreen"
-        app_with_users.screenManager.current_screen.onPressed()
-
-        await asyncio.sleep(0.5)
-        app_with_users.screenManager.RFIDReader.triggerFakeRead(card_id=123456789)
-
-        assert app_with_users.screenManager.current == "mainUserPage"
-        assert (
-            "User2FirstName"
-            in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
-        )
+    assert app_with_users.screenManager.current == "mainUserPage"
+    assert (
+        "User2FirstName"
+        in app_with_users.screenManager.current_screen.ids.header.ids.welcomeTextLabel.text
+    )
